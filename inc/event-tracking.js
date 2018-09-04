8e824c47a970d130a90892b3e1bf6911154a690f
DuckDivers <howarde@scubaboard.com>
2018-09-04 19:19:25 -0400

    all data-events

--- inc/event-tracking.js
+++ inc/event-tracking.js
@@ -1,6 +1,6 @@
 // Google Tracking 
 jQuery(document).ready(function($){
-    $('a[data-event="true"]').click(function(){
+    $('[data-event="true"]').click(function(){
         var action = $(this).data('action');
         var category = $(this).data('category');
         var label = $(this).data('label');
